25967a807eca38d08baeaab31e61712e50b98b65
vijeth_simha <vijethsimha71@gmail.com>
2023-09-24 17:23:35 +0530

    added icons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
+        title: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          const Text("Good Moring"),
+          Text(
+            'Vijeth Simha',
+            style: Theme.of(context).textTheme.labelMedium,
+          )
+        ]),
+        actions: [
+          IconButton(
+              onPressed: () {}, icon: const Icon(Ionicons.search_outline)),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(Ionicons.notifications_outline)),
+        ],
+      ),
+    );
   }
 }
