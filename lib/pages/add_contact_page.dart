b18325604842aefa120d52647d023c727d9a35a2
vijeth_simha <vijethsimha71@gmail.com>
2023-10-07 23:43:20 +0530

    add contact page is done

---
+++ lib/pages/add_contact_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddContactPage extends StatelessWidget {
+  const AddContactPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Contact"),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,28 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_playground/pages/add_contact_page.dart';
+import 'package:iconly/iconly.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Contact"),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AddContactPage(),
+            ),
+          );
+        },
+        label: const Text("Add Contact"),
+        icon: const Icon(IconlyBroken.document),
+      ),
+    );
   }
 }
