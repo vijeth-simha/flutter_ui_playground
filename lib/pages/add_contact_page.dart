16a935bed008906b5bb92a8210f98fafa6a6a266
vijeth_simha <vijethsimha71@gmail.com>
2023-10-08 00:11:36 +0530

    added text fields with controllers

--- lib/pages/add_contact_page.dart
+++ lib/pages/add_contact_page.dart
@@ -1,13 +1,75 @@
 import 'package:flutter/material.dart';
 
 class AddContactPage extends StatelessWidget {
-  const AddContactPage({super.key});
+  final nameController = TextEditingController();
+  AddContactPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Contact"),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(10.0),
+        children: [
+          Form(
+              child: Column(
+            children: [
+              TextFormField(
+                controller: nameController,
+                textInputAction: TextInputAction.next,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please enter a name";
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  hintText: "Name",
+                  // contentPadding: inputp,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: nameController,
+                textInputAction: TextInputAction.next,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please enter a name";
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  hintText: "Name",
+                  // contentPadding: inputp,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: nameController,
+                textInputAction: TextInputAction.next,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please enter a name";
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  hintText: "Name",
+                  // contentPadding: inputp,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ))
+        ],
       ),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,7 +16,7 @@ class HomePage extends StatelessWidget {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => const AddContactPage(),
+              builder: (context) => AddContactPage(),
             ),
           );
         },
