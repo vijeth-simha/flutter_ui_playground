3d6287824bd271a3499f4ed5b1a29f5c81cf09da
vijeth_simha <vijethsimha71@gmail.com>
2023-10-09 23:37:01 +0530

    added const

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,7 +16,7 @@ class HomePage extends StatelessWidget {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => AddContactPage(),
+              builder: (context) => const AddContactPage(),
             ),
           );
         },
