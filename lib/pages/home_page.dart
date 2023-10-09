9169f2019aa59fcf58814fc9de96a6ad7d57f9e2
vijeth_simha <vijethsimha71@gmail.com>
2023-10-10 00:11:14 +0530

    added const

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_playground/pages/add_contact_page.dart';
 import 'package:iconly/iconly.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final contactCollection =
+      FirebaseFirestore.instance.collection("contact").snapshots();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Contact"),
       ),
+      body: StreamBuilder(
+          stream: contactCollection,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const Text("Has Data");
+            } else if (snapshot.hasError) {
+              return const Text("Has error");
+            }
+            return const Center(
+              child: CircularProgressIndicator.adaptive(),
+            );
+          }),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           Navigator.push(
